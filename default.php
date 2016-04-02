aa38afdf14ba14bcdbc190ad9adeeea5f4e2bb0a
Alexandre Seiler <alexandresseiler@gmail.com>
2016-04-02 16:50:06 -0300

    update default.php

--- default.php
+++ default.php
@@ -1,9 +1,12 @@
-<<!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
 	<title>Jaqueline Maria</title>
 </head>
 <body>
 	<center><h1>Teste:</h1></center>
+<input type="text" id="Login"></input>
+<input type="password" id="Login"></input>
+
 </body>
-</html>>
+</html>
